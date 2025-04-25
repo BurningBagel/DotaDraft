6328e6fccf4d6c08097c31c5dc637e48128b6d13
Luiz <luiz.duarte11@yahoo.com.br>
2025-04-25 16:42:32 -0300

    Get specific hero and hero by strength

--- backend/src/heroes/heroes.controller.ts
+++ backend/src/heroes/heroes.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Query } from '@nestjs/common';
 import { HeroesService } from './heroes.service';
-
+import { Hero } from 'shared/models/hero.model';
 
 @Controller('heroes')
 export class HeroesController {
-    constructor(private readonly heroesService: HeroesService){}
+  constructor(private readonly heroesService: HeroesService) {}
 
-    @Get()
-    getHeroes(){
-        return this.heroesService.getHeroes();
+  @Get()
+  getHeroes() {
+    return this.heroesService.getHeroes();
+  }
+
+  @Get(':name')
+  getSpecificHero(@Param('name') name: string): Hero {
+    try {
+        return this.heroesService.getSpecificHero(name);
+        
+    } catch (error) {
+        throw new NotFoundException(`Hero ${name} not found!`)
     }
+  }
+
+  @Get('search')
+  getHeroesByStrengths(@Query('strengths') traits: string[]): Hero[] {
+    return this.heroesService.getHeroesByStrengths(traits);
+  }
 }
--- backend/src/heroes/heroes.service.ts
+++ backend/src/heroes/heroes.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
+import { Hero } from 'shared/models/hero.model';
 
 @Injectable()
 export class HeroesService {
-    private heroes: any[];
+  private heroes: Hero[];
 
-    constructor(){
-        const filePath = path.join(__dirname,'../../src/data/heroes.json');
-        console.log(filePath)
-        const fileContents = fs.readFileSync(filePath,'utf-8');
-        this.heroes = JSON.parse(fileContents);
-    }
+  constructor() {
+    const filePath = path.join(process.cwd(), 'src','data','heroes.json');
+    const fileContents = fs.readFileSync(filePath, 'utf-8');
+    this.heroes = JSON.parse(fileContents);
+  }
 
-    getHeroes(){
-        return this.heroes;
-    }
+  getHeroes() {
+    return this.heroes;
+  }
+
+  //searches through the list of heroes and returns true as soon as it finds one with the same name as the argument
+  getSpecificHero(name: string): any {
+    return this.heroes.find(
+      (item) => String(item.name).toUpperCase() == name.toUpperCase(),
+    );
+  }
+
+  //returns a filtered list of heroes, where each hero has at least 1 of the strengths sent inside traits
+  getHeroesByStrengths(traits: string[]): Hero[] {
+    return this.heroes.filter((hero) => {
+      if (hero.strengths.some((strength) => traits.includes(strength)))
+        return true;
+    });
+  }
 }
