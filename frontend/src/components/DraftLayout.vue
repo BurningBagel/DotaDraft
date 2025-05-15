f2c44ec56d9ccdbc5d26fe906a468ee5ec302d08
Luiz Duarte <luiz.duarte11@yahoo.com.br>
2025-05-15 14:43:38 -0300

    Team selection and Reset

--- frontend/src/components/DraftLayout.vue
+++ frontend/src/components/DraftLayout.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="container">
     <div class="RADIANT">
-      <TeamHeroes :team="radiant" :chosenTeam="'radiant'"/>
+      <TeamHeroes :team="radiant" :chosenTeam="'radiant'" />
     </div>
     <div class="DIRE">
-      <TeamHeroes :team="dire" :chosenTeam="'dire'"/>
+      <TeamHeroes :team="dire" :chosenTeam="'dire'" />
     </div>
-    <div class="SELECT-DIRE">
-      <TeamButton :teamName="'dire'" :TeamSelect="TeamSelect"/>
+
+    <div class="SELECT-DIRE" v-if="!selectedTeam">
+      <TeamButton :teamName="'dire'" :TeamSelect="TeamSelect" />
     </div>
-    <div class="SELECT-RADIANT">
-      <TeamButton :teamName="'radiant'" :TeamSelect="TeamSelect"/>
+    <div class="SELECT-RADIANT" v-if="!selectedTeam">
+      <TeamButton :teamName="'radiant'" :TeamSelect="TeamSelect" />
     </div>
+
+
+    <button @click="ResetDraft" v-if="selectedTeam" class="RESET">Reset</button>
+
     <div class="STR">
       <HeroGroup :title="'Strength'" :heroes="strHeroes" :OnSelect="AddHero" />
     </div>
     <div class="AGI">
       <HeroGroup :title="'Agility'" :heroes="agiHeroes" :OnSelect="AddHero" />
     </div>
-    
+
     <div class="INFO">
       <h3>Welcome to the Dota Draft Assistant!</h3>
-      
+
       <ul>
         <li>Select your Team choosing one of the buttons above</li>
         <li>Use Left Click to add a Hero to the Radiant or Right Click to add to the Dire</li>
         <li>Click again on any hero to remove them from that team</li>
-        <li>Below here you'll see recommendations on heroes you might want to pick based on your enemies strengths, as well as items you'll want to consider picking up!</li>
+        <li>Below here you'll see recommendations on heroes you might want to pick based on your enemies strengths, as
+          well as items you'll want to consider picking up!</li>
       </ul>
-        
+
     </div>
-    
+
     <div class="INT">
       <HeroGroup :title="'Intelligence'" :heroes="intHeroes" :OnSelect="AddHero" />
     </div>
@@ -49,30 +55,37 @@ import type { Hero } from "../shared/models/hero.model.ts";
 import { ref } from "vue";
 import TeamHeroes from "./TeamHeroes.vue";
 import TeamButton from "./TeamButton.vue";
+import type { Team } from "../shared/enums/team.enum.ts";
 
 const radiant = ref<Hero[]>([]);
 const dire = ref<Hero[]>([]);
 
-var selectedTeam: String; 
+const selectedTeam = ref<Team | null>(null);
 
-const TeamSelect = (team: String) => {
-  selectedTeam = team;
+const TeamSelect = (team: Team) => {
+  selectedTeam.value = team;
   // TODO hide the team buttons
+}
+
+const ResetDraft = () => {
+  selectedTeam.value = null;
+  radiant.value = [];
+  dire.value = [];
 }
 
 const AddHero = (team: String, hero: Hero) => {
   const chosenTeam = team === "radiant" ? radiant.value : dire.value;
   const otherTeam = team === "radiant" ? dire.value : radiant.value;
-  
-  if (!chosenTeam.includes(hero) && chosenTeam.length <= 5){
+
+  if (!chosenTeam.includes(hero) && chosenTeam.length <= 5) {
     chosenTeam.push(hero);
   }
-  else{
-    chosenTeam.splice(chosenTeam.findIndex((item) => {return item.name == hero.name}),1)
+  else {
+    chosenTeam.splice(chosenTeam.findIndex((item) => { return item.name == hero.name }), 1)
   }
 
-  if(otherTeam.includes(hero)){
-    otherTeam.splice(otherTeam.findIndex((item) => {return item.name == hero.name}),1)
+  if (otherTeam.includes(hero)) {
+    otherTeam.splice(otherTeam.findIndex((item) => { return item.name == hero.name }), 1)
   }
 };
 
@@ -113,35 +126,40 @@ const uniHeroes = computed(() =>
     ". STR STR . AGI AGI INFO INFO INT INT . UNI UNI .";
 }
 
-ul{
+ul {
   padding-left: 0.5rem;
 }
 
 
-.INFO{
+.INFO {
   grid-area: INFO;
   text-align: center;
 }
 
+.RESET{
+  grid-area: SELECT-RADIANT / SELECT-RADIANT / SELECT-DIRE / SELECT-DIRE;
+  align-items: center;
+  border: 1px solid yellow;
+  display: flex;
+  justify-content: center;
+}
 
 .RADIANT {
   grid-area: RADIANT;
-  border: 1px solid green;
 }
 
 .DIRE {
   grid-area: DIRE;
-  border: 1px solid red;
 }
 
 .SELECT-DIRE {
+  border: 1px solid red;
   grid-area: SELECT-DIRE;
-  border: 1px solid yellow;
 }
 
 .SELECT-RADIANT {
   grid-area: SELECT-RADIANT;
-  border: 1px solid orange;
+  border: 1px solid green;
 }
 
 .STR {
